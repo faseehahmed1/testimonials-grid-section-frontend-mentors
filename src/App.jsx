6d2e188e4bfa2bb09b37a55f054bd337d5f8245c
faseehahmed1 <faseeh_455@hotmail.com>
2023-08-26 21:35:24 +0100

    mvp done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import clsx from "clsx";
+import classes from "./App.module.css";
+import DATA from "./DATA.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={classes.container}>
+      {DATA.map((person, index) => {
+        const cardClass = clsx([classes.card], {
+          [classes.grid_span_2]: person.span,
+          [classes.grid_span_row_2]: person.spanRow,
+          [classes.gray]: person.background === "grey",
+          [classes.white]: person.background === "white",
+          [classes.darkgrey]: person.background === "darkgrey",
+        });
+
+        const headingClass = clsx([classes.heading], {
+          [classes.heading_white]: person.background === "white",
+        });
+
+        const nameClass = clsx({
+          [classes.name_white]: person.background === "white",
+        });
+
+        const roleClass = clsx({
+          [classes.role_white]: person.background === "white",
+        });
+        return (
+          <div key={index} className={cardClass}>
+            <div className={classes.profile}>
+              <img
+                className={classes.picture}
+                src={person.image}
+                alt={`profile pic of daniel`}
+              />
+              <div className={classes.profile_info}>
+                <h3 className={nameClass}>{person.name}</h3>
+                <h4 className={roleClass}>Verified Graduate</h4>
+              </div>
+            </div>
+            <h1 className={headingClass}>{person.heading}</h1>
+            <h2 className={nameClass}>" {person.description} "</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
